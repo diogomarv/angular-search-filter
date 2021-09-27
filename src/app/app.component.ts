3aefa82d9eca2530e2b1cc35792d1d0e51a5f0d2
diogomarv <diogomarv@gmail.com>
2021-09-26 22:51:31 -0300

    Change of type: .css to .scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { LocalEventsDTO } from './DTOs/LocalEventsDTO';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
 
